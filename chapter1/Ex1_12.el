511d209169b2e36d5196ec9ec1ac0050b8104092
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-07-15 09:01:40 -0700

    so far

--- chapter1/Ex1_12.el
+++ chapter1/Ex1_12.el
@@ -1,7 +1,8 @@
 ;; -*- lexical-binding: t -*-
 ;; Pascal triangle
+(require 'seq)
+(seq-reduce (lambda(acc x)(+ acc x)) '(1 2 3 4) 0)
 
 (defun helper (n prev-row))
 
-(defun pascal-triangle (n)
-  )
+(defun pascal-triangle (n))
