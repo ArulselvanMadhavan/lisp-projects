d50050c8f0c88fdabc8b69977f8ec66e6f81a2c8
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-09-30 10:39:46 -0700

    Finished Chapter 4

--- little-typer/chapter4/playground.rkt
+++ little-typer/chapter4/playground.rkt
@@ -1,19 +1,5 @@
 #lang pie
 
-(claim kar
-       (-> (Pair Nat Nat)
-           Nat))
-(define kar
-  (λ (p)
-    3))
-
-(claim swap
-       (-> (Pair Nat Atom)
-           (Pair Atom Nat)))
-(define swap
-  (λ (p)
-    (cons 'test 4)))
-
 (claim flip
        (Π ((A U)
            (D U))
@@ -23,3 +9,58 @@
   (λ (A D)
     (λ (p)
       (cons (cdr p) (car p)))))
+
+(claim elim-pair
+       (Π ((A U)
+           (D U)
+           (X U))
+          (-> (Pair A D)
+              (-> A D
+                  X)
+              X)))
+(define elim-pair
+  (λ (A D X)
+    (λ (p f)
+      (f (car p) (cdr p)))))
+
+(claim kar
+       (-> (Pair Nat Nat)
+           Nat))
+(define kar
+  (λ (p)
+    (elim-pair Nat Nat
+               Nat
+               p
+               (λ (a d)
+                 a))))
+
+(claim kdr
+       (-> (Pair Nat Nat)
+           Nat))
+(define kdr
+  (λ (p)
+    (elim-pair Nat Nat
+               Nat
+               p
+               (λ (a d)
+                 d))))
+
+(claim swap
+       (-> (Pair Nat Atom)
+           (Pair Atom Nat)))
+(define swap
+  (λ (p)
+    (elim-pair Nat Atom
+               (Pair Atom Nat)
+               p
+               (λ (a d)
+                 (cons d a)))))
+
+(claim twin
+       (Π ((A U))
+          (-> A
+              (Pair A A))))
+(define twin
+  (λ (A)
+    (λ (a)
+      (cons a a))))
