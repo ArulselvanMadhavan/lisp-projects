956c0392a60e87b63c24f68cda083b9793a18ffc
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-09-30 20:59:24 -0700

    Finished chapter 5

--- little-typer/chapter5/playground.rkt
+++ little-typer/chapter5/playground.rkt
@@ -58,3 +58,78 @@
       (rec-List xs
                 ys
                 (step-append E)))))
+
+(claim snoc
+       (Π ((E U))
+          (-> (List E) E
+              (List E))))
+(define snoc
+  (λ (E)
+    (λ (start e)
+      (rec-List start
+                (:: e nil)
+                (step-append E)))))
+
+(claim iter-List
+       (Π ((E U)
+           (X U))
+          (-> (List E) X (-> E (List E) X)
+              X)))
+(define iter-List
+  (λ (E X)
+    (λ (es b f)
+      (rec-List es
+                b
+                (λ (e es rec-n-1)
+                  (f e es))))))
+
+(claim step-reverse
+       (Π ((E U))
+          (-> E (List E) (List E)
+              (List E))))
+(define step-reverse
+  (λ (E)
+    (λ (e es reverse-es)
+      (snoc E reverse-es e))))
+
+(claim reverse
+       (Π ((E U))
+          (-> (List E)
+              (List E))))
+(define reverse
+  (λ (E)
+    (λ (es)
+      (rec-List es
+                (the (List E) nil)
+                (step-reverse E)))))
+
+
+(claim step-concat
+       (Π ((E U))
+          (-> E (List E) (List E)
+              (List E))))
+(define step-concat
+  (λ (E)
+    (λ (e es concat-es)
+      (snoc E concat-es e))))
+
+(claim concat
+       (Π ((E U))
+          (-> (List E) (List E)
+              (List E))))
+(define concat
+  (λ (E)
+    (λ (xs ys)
+      (rec-List (reverse E ys)
+                xs
+                (step-concat E)))))
+
+(claim kartoffelmad
+       (List Atom))
+(define kartoffelmad
+  (append Atom
+          (concat Atom
+                  toppings condiments)
+          (reverse Atom
+                   (:: 'plate
+                       (:: 'rye-bread nil)))))
