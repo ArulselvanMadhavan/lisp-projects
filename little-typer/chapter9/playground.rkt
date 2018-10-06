f536179fee148e5aa730d65a9c4319366cb026de
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-10-05 22:38:58 -0700

    Some more stuff

--- little-typer/chapter9/playground.rkt
+++ little-typer/chapter9/playground.rkt
@@ -74,3 +74,93 @@
              mot-incr=add1
              base-incr=add1
              step-incr=add1)))
+
+;; Double = twice
+
+(claim double
+       (-> Nat
+           Nat))
+(define double
+  (λ (n)
+    (iter-Nat n
+              0
+              (+ 2))))
+
+(double 2)
+(iter-Nat 3
+          0
+          (+ 2))
+
+(+ 2 (iter-Nat 2
+               0
+               (+ 2)))
+(+ 2 (+ 2 (iter-Nat 1
+                    0
+                    (+ 2))))
+(+ 2 (+ 2 (+ 2 (iter-Nat 0
+                         0
+                         (+ 2)))))
+
+(claim twice
+       (-> Nat
+           Nat))
+(define twice
+  (λ (n)
+    (+ n n)))
+(twice 3)
+(iter-Nat 3
+          3
+          (λ (+n-1)
+            (add1 +n-1)))
+(add1 (add1 (add1 3)))
+
+;; (claim twice=double
+;;        (Π ((n Nat))
+;;           (= Nat (twice n) (double n))))
+;; (define twice=double
+;;   (λ (n)
+;;     ))
+
+(claim mot-add1+=+add1
+       (-> Nat Nat
+           U))
+(define mot-add1+=+add1
+  (λ (j k)
+    (= Nat
+       (add1 (+ k j))
+       (+ k (add1 j)))))
+
+(claim step-add1+=+add1
+       (Π ((j Nat)
+           (n-1 Nat))
+          (-> (mot-add1+=+add1 j n-1)
+              (mot-add1+=+add1 j (add1 n-1)))))
+(define step-add1+=+add1
+  (λ (j n-1)
+    (λ (add1+=+add1)
+      (cong add1+=+add1
+            (+ 1)))))
+
+(claim add1+=+add1
+       (Π ((n Nat)
+           (j Nat))
+          (= Nat
+             (add1 (+ n j))
+             (+ n (add1 j)))))
+(define add1+=+add1
+  (λ (n j)
+    (ind-Nat n
+             (mot-add1+=+add1 j)
+             (same (add1 j))
+             (step-add1+=+add1 j))))
+
+
+;; (= Nat
+;;    (add1 (+ (add1 n-1) j))
+;;    (+ (add1 n-1) (add1 j)))
+;; (= Nat
+;;    (add1 (add1 (+ n-1 j)))
+;;    (add1 (+ n-1 (add1 j))))
+
+
+;; (mot-add1+=+add1 3 4)
