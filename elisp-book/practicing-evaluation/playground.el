238c7166bd747ee190c09f8c46ae6364c4ae8eb3
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-12-22 00:51:19 -0800

    Started the elisp book

---
+++ elisp-book/practicing-evaluation/playground.el
@@ -0,0 +1,10 @@
+;; parenthesis tell the interpreter to treat the enclosed content as function and not as variables.
+
+;; C-u C-x C-e
+(buffer-name)"playground.el"
+(buffer-file-name)"/home/arul/dev/lisp-projects/elisp-book/practicing-evaluation/playground.el"
+
+;; Getting the actual buffer
+(current-buffer)
+;; Get the other buffer(that is not in the frame)
+(other-buffer)
